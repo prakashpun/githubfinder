49a19bece53848d1905e1864b863a5321c778ca7
prakashpun <prakash.pun@gmail.com>
2019-07-01 15:59:52 +0530

    Display 5 latest public repos

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "5dc6d88afb0ef1920bc3";
     this.client_secret = "7495c5cb18557c9421ee13dc0e46ed0ea8b649aa";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -11,9 +13,19 @@ class Github {
       }&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
